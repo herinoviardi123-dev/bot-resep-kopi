8048995f1825e48db14e1af953c33384851c920d
herinoviardi123-dev <herinoviardi123@gmail.com>
2026-04-15 19:06:18 +0700

    Update bot_resep_kopi.py

--- bot_resep_kopi.py
+++ bot_resep_kopi.py
@@ -29,7 +29,7 @@ import anthropic
 
 # ===================== KONFIGURASI =====================
 # Dibaca otomatis dari Railway Environment Variables
-BOT_TOKEN = "8666305875:AAFyZojgPibD_kOK1nA1wBsxNGcKOB75-3E"
+BOT_TOKEN = "8666305875:AAGvLc3NHV7GFU0qP-tx6KVztcbcjyooOf8"
 ANTHROPIC_API_KEY = "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"
 WEBAPP_URL = "https://iridescent-otter-0b20a1.netlify.app/"  # contoh: https://amazing-coffee-123.netlify.app
 
